5d41e8e6585debae8f87b23945c26fc77693a71c
Nikita Gryzlov <nixel2007@gmail.com>
2018-02-23 19:37:09 +0300

    Add $schema params on json config files generation

--- src/templates/sonarlintTemplate.ts
+++ src/templates/sonarlintTemplate.ts
@@ -4,6 +4,7 @@ export default class SonarlintTemplate implements IConfigTemplate {
 
     public getTemplateObject(): object {
         return {
+            $schema: "https://raw.githubusercontent.com/silverbulleters/sonarqube-inject-vsc/master/schemas/sonarlint.json",
             serverId: "my-company-server",
             projectKey: "my-project",
         };
